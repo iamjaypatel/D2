4f36de158441985226808d225f31d43c89581fe6
Jay Patel <jay26597@gmail.com>
2019-02-27 18:21:51 -0500

    added few more test cases

--- checkArguments_test.rb
+++ checkArguments_test.rb
@@ -5,7 +5,20 @@ require_relative 'ruby_rush'
 # Classes to test checkArguments class
 class checkArgumentsTest < Minitest::Test
 
-  def test_check_one_invalid_arguments
+  # Test that No Arguments fail
+  def test_check_invalid_arguments
     args = CheckArguments.new
     assert_equal false, args.check_Arguments([])
+  end
+
+  # Test that atleast one argument fails
+  def test_check_one_invalid_argument
+    args = CheckArguments.new
+    assert_equal false, args.checkArguments([1])
+  end
+
+  # Test that atleast one argument fails
+  def test_check_two_invalid_argument
+    args = CheckArguments.new
+    assert_equal false, args.checkArguments([2])
   end
